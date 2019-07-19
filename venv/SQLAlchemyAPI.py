10038ea5e2a010036d2548ba278b0d5866e5c6d9
mahdaouimoatassim <ce_mahdaoui@esi.dz>
2019-07-19 11:52:30 +0200

    Ajouter les modifications permettant de traiter 25 CV (En cours)3

--- venv/SQLAlchemyAPI.py
+++ venv/SQLAlchemyAPI.py
@@ -11,9 +11,9 @@ from dateutil import relativedelta
 import numpy as np
 
 utilisateur="postgres"
-mot_de_passe="Data"
+mot_de_passe="postgresql123"
 serveur="localhost"
-port=5432
+port=5433
 base_de_donnees="magic_skill"
 lien_connexion="postgresql://"+utilisateur+":"+mot_de_passe+"@"+serveur+":"+str(port)+"/"+base_de_donnees
 
